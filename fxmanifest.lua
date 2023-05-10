f7f7875e5169deba87470e78a980f09817c463d4
frackz <65610875+frackz@users.noreply.github.com>
2023-05-10 21:16:44 +0200

    asd

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'frackz <https://github.com/frackz>'
+description 'A FiveM framework'
+version '1.0.0'
+
+-- What to run
+shared_scripts {
+
+}
+
+server_scripts {
+
+}
+
+client_scripts {
+    
+}
+
+lua54 'yes'
