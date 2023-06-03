b88f50dacf7aab3aec09fd3942555317b525e19c
frackz <65610875+frackz@users.noreply.github.com>
2023-06-03 16:27:39 +0200

    asd

--- client/GUI/Prompt.lua
+++ client/GUI/Prompt.lua
@@ -1,7 +1,7 @@
 -- Prompt controller for Spark.
 -- Made and maintained by frackz
 
-local NUI, Status, Prompt = Spark:NUI(), false, {}
+local NUI, Status, Prompt, Callback = Spark:NUI(), false, {}, nil
 
 --- Get the prompt module
 function Spark:Prompt()
@@ -11,8 +11,11 @@ end
 --- Show the prompt
 --- @param text string
 --- @param size number
-function Prompt:Show(text, size)
+function Prompt:Show(text, size, callback)
     assert(not Status, "Prompt is already open")
+    assert(type(Callback) == "function", "Callback is not a function")
+
+    Callback, Status = callback, true
     return NUI:Focus(true, true), NUI:Send({
         show = true,
         text = text,
@@ -25,12 +28,17 @@ end
 --- When the key / button cancel has been pressed
 NUI:Register('Prompt:Cancel', function()
     NUI:Focus(false, false)
+    Callback(false)
+
+    Status, Callback = false, nil
     return true
 end)
 
 --- When the key / button submit has been pressed
 NUI:Register('Prompt:Submit', function(data)
     NUI:Focus(false, false)
-    print(data.text)
+    Callback(true, data.text)
+
+    Status, Callback = false, nil
     return true
 end)
